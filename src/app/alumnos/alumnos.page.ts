887b8c54a70a738eda05997687935fe41411783a
Alan Barrera <alangabriel49@gmail.com>
2025-04-22 13:17:06 -0400

    arraya alumnos para firebase

--- src/app/alumnos/alumnos.page.ts
+++ src/app/alumnos/alumnos.page.ts
@@ -1,18 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { IonAccordion, IonAccordionGroup, IonItem, IonLabel,IonHeader,IonToolbar,IonTitle,IonContent } from '@ionic/angular/standalone';
+import { IonAccordion, IonAccordionGroup, IonItem, IonLabel,IonHeader,IonToolbar,IonTitle,IonContent,IonButton,IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { personCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-alumnos',
   templateUrl: './alumnos.page.html',
   styleUrls: ['./alumnos.page.scss'],
   standalone: true,
-  imports: [IonAccordion, IonAccordionGroup, IonItem, IonLabel,IonHeader,IonToolbar,IonTitle,IonContent],
+  imports: [CommonModule ,IonAccordion, IonAccordionGroup, IonItem, IonLabel,IonHeader,IonToolbar,IonTitle,IonContent,IonButton,IonIcon],
 })
-export class AlumnosPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+// HAY QUE CAMBIAR LAS ID PARA LOS ALUMNOS 
+// HAY QUE VER QUE CONTENIDO SE AGREGARA A CADA ALUMNO 
+export class AlumnosPage {
+  alumnos = [
+    {
+      id: 'first',
+      nombre: 'Alan Barrera',
+      contenido: 'First Content',
+      expanded: false
+    },
+    {
+      id: 'second',
+      nombre: 'José Calderón',
+      contenido: 'Second Content',
+      expanded: false
+    },
+    {
+      id: 'third',
+      nombre: 'Carlos Araya',
+      contenido: 'Third Content',
+      expanded: false
+    },
+    {
+      id: 'fourth',
+      nombre: 'Andres Carranza',
+      contenido: 'Third Content',
+      expanded: false
+    },
+    {
+      id: 'five',
+      nombre: 'Gina Nexlson',
+      contenido: 'GOOOOORDAAAAAAA',
+      expanded: false
+    }
+  ]
+  constructor() {
+    addIcons({ personCircleOutline }); 
   }
-
 }
